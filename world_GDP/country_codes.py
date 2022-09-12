7ed2ed0f2caa87751fba7c9a4f41bb21e57b6f96
Alex <71852620+Alexander2424@users.noreply.github.com>
2022-09-12 12:36:58 +0100

    Update country_codes.py

--- world_GDP/country_codes.py
+++ world_GDP/country_codes.py
@@ -43,8 +43,3 @@ def get_country_code(country_name):
 			return 'cd'
 	# If the country wasn't found, return None.
 	return None
-
-# print(get_country_code('Andorra'))
-# print(get_country_code('United Arab Emirates'))
-# print(get_country_code('Afghanistan'))
-
